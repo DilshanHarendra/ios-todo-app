b2eacd12a14a69e91e8e583d460c04fe7e82e0a6
DilshanHarendra <harendracripto@gmail.com>
2021-12-09 11:29:49 +0530

    modify taskModel

--- Todo_App/models/TaskModel.swift
+++ Todo_App/models/TaskModel.swift
@@ -9,8 +9,8 @@ import Foundation
 import SQLite3
 class TaskModel {
     
-    func createTask(task:Task) -> String {
-        var message:String = ""
+    func createTask(task:Task) -> Task {
+        var newTask:Task? = nil
             
         let insertQuery="INSERT INTO tasks (task, description, startsAt, endsAt) VALUES (?,?,?,?);"
         var insertStatement: OpaquePointer?=nil
@@ -22,17 +22,19 @@ class TaskModel {
             sqlite3_bind_text(insertStatement,4, (task.getEndAt() as NSString).utf8String, -1, nil)
          
             if sqlite3_step(insertStatement) == SQLITE_DONE {
-                message="TASK add successfull"
+                let lastRowId = sqlite3_last_insert_rowid(DatabaseService.db)
+                print("TASK add successfull")
+                newTask = Task(id: Int(lastRowId), task: task.getTask(), description: task.getDescription(), startAt: task.getStartAt(), endsAt: task.getEndAt())
             }else{
-               message="TASK cannot add"
+               print("TASK cannot add")
             }
             
         }else{
-            message = "Task INSERT statement could not be prepared"
+            print("Task INSERT statement could not be prepared")
         }
         sqlite3_finalize(insertStatement)
         
-        return message
+        return newTask!
     }
     
     func getTasks() -> [Task] {
@@ -41,21 +43,18 @@ class TaskModel {
         var selectStatement: OpaquePointer? = nil
         
         if sqlite3_prepare_v2(DatabaseService.db, selectQuery, -1, &selectStatement, nil) == SQLITE_OK {
-            if sqlite3_step(selectStatement) == SQLITE_DONE {
-                print("TASK SELECT DONE")
-               while sqlite3_step(selectStatement) == SQLITE_ROW {
-                    let id = sqlite3_column_int(selectStatement, 0)
-                    let task = String(describing: String(cString: sqlite3_column_text(selectStatement, 1)))
-                    let description = String(describing: String(cString: sqlite3_column_text(selectStatement, 2)))
-                    let startsAt = String(describing: String(cString: sqlite3_column_text(selectStatement,3)))
-                    let endsAt = String(describing: String(cString: sqlite3_column_text(selectStatement,4)))
+          
+            while sqlite3_step(selectStatement) == SQLITE_ROW {
+                let id = sqlite3_column_int(selectStatement, 0)
+                let task = String(describing: String(cString: sqlite3_column_text(selectStatement, 1)))
+                let description = String(describing: String(cString: sqlite3_column_text(selectStatement, 2)))
+                let startsAt = String(describing: String(cString: sqlite3_column_text(selectStatement,3)))
+                let endsAt = String(describing: String(cString: sqlite3_column_text(selectStatement,4)))
                     
-                    tasks.append(Task(id: Int(id), task: task, description: description, startAt: startsAt, endsAt: endsAt))
-                    print("TASK SELECT DONE")
-                }
-            }else{
-                print("TASK cannot be read")
+                tasks.append(Task(id: Int(id), task: task, description: description, startAt: startsAt, endsAt: endsAt))
+                print("TASK SELECT DONE")
             }
+            
             
         }else{
             print("Task SELECT statement could not be prepared")
